ceac6bba5ebc06aaf08ffbe2ecb385a86130f83b
Danielss89 <Danielss89@gmail.com>
2015-06-04 20:03:05 +0200

    Updated docblock

--- src/View/Helper/MoneyFormat.php
+++ src/View/Helper/MoneyFormat.php
@@ -12,12 +12,16 @@ use Money\Money;
 class MoneyFormat extends AbstractHelper
 {
     /**
-     * @param Money $money
+     * @param  Money  $money
+     * @param  bool   $showDecimals
+     * @param  string $locale
+     * @param  string $pattern
      * @return string
      */
     public function __invoke(Money $money, $showDecimals = null, $locale = null, $pattern = null)
     {
         $currencyFormat = $this->getView()->plugin('currencyFormat');
+        $currencyFormat->setCurrencyPattern($pattern);
         
         return $currencyFormat($money->getAmount() / 100, $money->getCurrency(), $showDecimals, $locale, $pattern);
     }
